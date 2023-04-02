b3989b5192ba2f3029a7af874922ef8dbbf7ce70
Prince Francis <prince.francis64@gmail.com>
2023-04-02 12:44:00 +0530

    commit 2

---
+++ test3.py
@@ -0,0 +1,17 @@
+import logging
+logging.basicConfig(filename="test3.log",level=logging.DEBUG,format="%(levelname)s %(name)s %(asctime)s %(message)s ")
+
+
+def divide(a,b):
+    logging.info("trying to divide %s and %s",a,b)
+
+    try:
+        logging.info("we are into a function")
+        div =a/b
+        logging.info("we have completed division operation")
+        logging.info("the result of code is %s",div)
+        return div
+    except Exception as e:
+        logging.exception(e)
+
+divide(4,0)
